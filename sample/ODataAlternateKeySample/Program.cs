c7eecc3fd9cacd2ddb2d0c29da4be7aebf17cd46
Jason Liu <jasonlhy@gmail.com>
2022-06-23 15:31:59 +0800

    Update to DI into OData Service

--- sample/ODataAlternateKeySample/Program.cs
+++ sample/ODataAlternateKeySample/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Formatter.Serialization;
 using ODataAlternateKeySample.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +7,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddTransient<IAlternateKeyRepository, AlternateKeyRepositoryInMemory>();
-builder.Services.AddControllers().
-    AddOData(opt => opt.EnableQueryFeatures()
-        .AddRouteComponents("odata", EdmModelBuilder.GetEdmModel()));
+
+// source code reference
+
+// ODataOutputFormatter.cs
+// ODataOptions.cs
+// HttpRequestExtensions.cs
+
+// Not work
+// Modify the global scope didn't affect the odata scope... idea why design like this
+// Please look at the source code in the source code at ODataOptions.cs
+// routeServiceProvider.CreateAsyncScope
+// builder.Services.AddSingleton<IODataSerializerProvider, IngoreNullEntityPropertiesSerializerProvider>();
+
+builder.Services.AddControllers()
+    .AddOData((opt, d) =>
+        {
+            // IServiceCollection services = new ServiceCollection();
+            var odataOptions = opt.EnableQueryFeatures()
+                .AddRouteComponents("odata", EdmModelBuilder.GetEdmModel(), (services) =>
+                {
+                    // this service is for odata scope only ... no idea why
+                    // by looking at the source code at ODataOptions.cs
+                    services.AddSingleton<IODataSerializerProvider, IngoreNullEntityPropertiesSerializerProvider>();
+                });
+            // debug
+            // var routeServiceProvider = odataOptions.GetRouteServices("odata");
+            //var serializerProvider = routeServiceProvider.GetRequiredService<IODataSerializerProvider>();
+        });
 
 var app = builder.Build();
 
